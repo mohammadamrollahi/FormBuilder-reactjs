6091912a412cc708f24f6b239504e52129e61182
Mohammad Amrollahi <mohammadamrollahi3@gmail.com>
2020-08-01 12:02:01 +0430

    create Data component

---
+++ src/components/GetData/Data.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function Data() {
+    let initialData={
+        organization_name:"",
+        organization_name_fa:"",
+        logo_url:"",
+        questione:[{
+            id:"",
+            text:"",
+            options:[
+                {key:1,text:""},
+                {key:1,text:""},
+                {key:1,text:""}
+
+            ],
+        }
+    ],
+    background_url:""
+    };
+    let outPut=[
+        {id:"",answer:1},
+        {id:"",answer:1},
+        {id:"",answer:1},
+    ]
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Data
