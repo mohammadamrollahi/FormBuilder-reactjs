d0959953b19ca70eb50f1196b3058df578eab0df
Mohammad Amrollahi <mohammadamrollahi3@gmail.com>
2020-08-08 19:00:53 +0430

    Updates

---
+++ precache-manifest.a4612b82f7874a193376ab0689bf49ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072948ac0273a933c1d3205c78a08db9",
+    "url": "/FormBuilder-reactjs/index.html"
+  },
+  {
+    "revision": "aac87e3b8dcb90144446",
+    "url": "/FormBuilder-reactjs/static/css/main.219687d2.chunk.css"
+  },
+  {
+    "revision": "8d3f5a3851ad858faa6b",
+    "url": "/FormBuilder-reactjs/static/js/2.c2394c6e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/FormBuilder-reactjs/static/js/2.c2394c6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aac87e3b8dcb90144446",
+    "url": "/FormBuilder-reactjs/static/js/main.cae4b8a0.chunk.js"
+  },
+  {
+    "revision": "297eefcb24d2c9063e58",
+    "url": "/FormBuilder-reactjs/static/js/runtime-main.622982cf.js"
+  },
+  {
+    "revision": "52ce4de2efeeb8b18dcbd379711224f3",
+    "url": "/FormBuilder-reactjs/static/media/Yekan.52ce4de2.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/FormBuilder-reactjs/static/media/logo.5d5d9eef.svg"
+  }
+]);
