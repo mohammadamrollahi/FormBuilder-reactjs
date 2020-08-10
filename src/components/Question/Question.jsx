cf372d9b47132c2c7444babb97a91dad758e954e
Mohammad Amrollahi <mohammadamrollahi3@gmail.com>
2020-08-10 23:46:05 +0430

    bugs fixed

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,23 +1,38 @@
-import React from 'react'
-import "./Question.scss"
-function Question({question,handleNext,output,setoutput}) {
-    const AddToOutput=(item)=>{
-       
-        setoutput([...output,{id:question.id,answer:item.key}])
+import React from "react";
+import "./Question.scss";
+function Question({ question, handleNext, output, setoutput }) {
+  const AddToOutput = (item) => {
+    let temp = true;
+    setoutput([...output.map((customItem) => {
+      if (customItem.id == question.id) {
+        customItem.answer = item.key;
+        temp = false;
+        return customItem
+      }
+      else return customItem
+    })])
+    if (temp) {
+      setoutput([...output, { id: question.id, answer: item.key }]);
     }
-    return (
-        <div>
-            <p className="questionText"> {question.text}</p>         
-            <div className="optionButtonDiv">
-            {question.options.map(item=>
-                <div className="optionButton" onClick={()=>{AddToOutput(item); handleNext()}} >
-                    {item.text}
-                    
-                </div>
-                )}
-            </div>
-        </div>
-    )
+  };
+  return (
+    <div className="questionDiv">
+      <p className="questionText"> {question.text}</p>
+      <div className="optionButtonDiv">
+        {question.options.map((item) => (
+          <div
+            className="optionButton"
+            onClick={() => {
+              AddToOutput(item);
+              handleNext();
+            }}
+          >
+            {item.text}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Question
+export default Question;
